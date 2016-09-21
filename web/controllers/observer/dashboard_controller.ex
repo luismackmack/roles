adffd750d7c9ac25e504b1ebe49c66b4355d12bf
Luis Ángel García Sierra <luisgarciasierra@gmail.com>
2016-09-21 06:16:37 -0400

    Update dashboard_controller.ex

--- web/controllers/observer/dashboard_controller.ex
+++ web/controllers/observer/dashboard_controller.ex
@@ -6,6 +6,7 @@ defmodule Roles.Observer.DashboardController do
 	  render conn, "index.html"
 	end
 
+        # We can do this function plug in module plugs  for let us define a connection transformation in other controllers
   	defp access_level(conn, _opts) do
   		rol_id = conn.assigns.current_user.rol_id
   		current_user_rol = Roles.Repo.get(Roles.Rol, rol_id)
